9c9abef70d32df455307dd341bc349642084d6a4
Niumlaque <niumlaque@gmail.com>
2022-12-14 16:59:31 +0900

    [vscode] Add an extension installation script for Windows

---
+++ vscode/install_vscode_extensions.bat
@@ -0,0 +1,5 @@
+call code --install-extension vscodevim.vim
+call code --install-extension MS-CEINTL.vscode-language-pack-ja
+call code --install-extension rust-lang.rust-analyzer
+call code --install-extension vadimcn.vscode-lldb
+call code --install-extension bungcip.better-toml
