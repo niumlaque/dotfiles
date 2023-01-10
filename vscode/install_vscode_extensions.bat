465c1ba2871fe415de829566159a77697fd2b965
Niumlaque <niumlaque@gmail.com>
2023-01-10 14:34:09 +0900

    [vscode] Add live server extension

--- vscode/install_vscode_extensions.bat
+++ vscode/install_vscode_extensions.bat
@@ -3,3 +3,4 @@ call code --install-extension MS-CEINTL.vscode-language-pack-ja
 call code --install-extension rust-lang.rust-analyzer
 call code --install-extension vadimcn.vscode-lldb
 call code --install-extension bungcip.better-toml
+call code --install-extension ritwickdey.liveserver
