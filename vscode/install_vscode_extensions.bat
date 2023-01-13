eb1e2d1cd78aa932daf4f2fd54dc74c952dbfa3b
Niumlaque <niumlaque@gmail.com>
2023-01-13 13:39:42 +0900

    [vscode] Add extensions for golang

--- vscode/install_vscode_extensions.bat
+++ vscode/install_vscode_extensions.bat
@@ -4,3 +4,5 @@ call code --install-extension rust-lang.rust-analyzer
 call code --install-extension vadimcn.vscode-lldb
 call code --install-extension bungcip.better-toml
 call code --install-extension ritwickdey.liveserver
+call code --install-extension golang.go
+call code --install-extension premparihar.gotestexplorer
