ca885ed52fe4e26944c286dc7733876e7f688a9d
Niumlaque <niumlaque@gmail.com>
2023-01-30 18:19:29 +0900

    [vscode] Add extension for markdown

--- vscode/install_vscode_extensions.bat
+++ vscode/install_vscode_extensions.bat
@@ -6,3 +6,4 @@ call code --install-extension bungcip.better-toml
 call code --install-extension ritwickdey.liveserver
 call code --install-extension golang.go
 call code --install-extension premparihar.gotestexplorer
+call code --install-extension bierner.markdown-preview-github-styles
